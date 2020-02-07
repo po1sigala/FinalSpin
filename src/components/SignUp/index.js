69543f6339d403ff5bd8d918b4e1fb54c5038d42
Your Name <alexsigala87@gmail.com>
2020-02-07 13:57:06 -0800

    built sign up page adding functionality to button to capture forms and send info to db

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
-
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Badge from "react-bootstrap/Badge";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 class Signup extends Component {
     render() {
-        return <div></div>;
+        return (
+            <div className="row">
+                <div className="col-lg-4 col-md-2 col-sm-1"></div>
+                <div className="col-lg-4 col-md-8 col-sm-10">
+                    <Card>
+                        <Card.Body>
+                            <Form>
+                                <div>
+                                    <Badge
+                                        variant="secondary"
+                                        style={{ backgroundColor: "black" }}
+                                    >
+                                        Sign Up
+                                    </Badge>
+                                </div>
+                                <Row>
+                                    <Col>
+                                        <Form.Control placeholder="First name" />
+                                    </Col>
+                                    <Col>
+                                        <Form.Control placeholder="Last name" />
+                                    </Col>
+                                </Row>
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Password"
+                                    />
+                                </Form.Group>
+                            </Form>
+                            <Button> Sign Up!</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-lg-4 col-md-2 col-sm-1"></div>
+            </div>
+        );
     }
 }
 export default Signup;
